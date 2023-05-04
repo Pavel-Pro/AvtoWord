a442406d22a7468f3ca245ff1d059a1a0449a358
Pavel-Pro <38453074+Pavel-Pro@users.noreply.github.com>
2023-05-04 15:08:24 +0300

    javascript
    Logic

--- js/main.js
+++ js/main.js
@@ -2,3 +2,29 @@ $(".burger").click(function() {
     $(".menu").toggleClass("show-menu");
     $(".burger__item").toggleClass("open");
 });
+$(".testimonials__items").slick({
+    slidesToShow: 2,
+    slideToScroll: 1,
+    arrows: true,
+    dots: true,
+    infinite: true,
+    responsive: [{
+        breakpoint: 770,
+        settings: {
+            slidesToShow: 1
+        }
+    }]
+});
+$(".gallery__item:nth-child(n+2)").click(function() {
+    $(".gallery__item:first-child").empty();
+    $(this).children().clone().appendTo(".gallery__item:first-child");
+});
+
+$(".button__item:last-child").click(function() {
+    $(".pop-up").css({"display" : "flex"});
+    $(".body").css({"position" : "fixed"});
+});
+$(".popup-close, .form-bg").click(function() {
+    $(".pop-up").css({"display" : "none"});
+    $(".body").css({"position" : "relarive"});
+});

4f650b809f314a360e2765f1dee4db5a47a85977
Pavel-Pro <38453074+Pavel-Pro@users.noreply.github.com>
2023-05-05 13:33:17 +0300

    pop-up
    pop-up on catalog

--- js/main.js
+++ js/main.js
@@ -2,19 +2,6 @@ $(".burger").click(function() {
     $(".menu").toggleClass("show-menu");
     $(".burger__item").toggleClass("open");
 });
-$(".testimonials__items").slick({
-    slidesToShow: 2,
-    slideToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true,
-    responsive: [{
-        breakpoint: 770,
-        settings: {
-            slidesToShow: 1
-        }
-    }]
-});
 $(".gallery__item:nth-child(n+2)").click(function() {
     $(".gallery__item:first-child").empty();
     $(this).children().clone().appendTo(".gallery__item:first-child");
@@ -27,4 +14,17 @@ $(".button__item:last-child").click(function() {
 $(".popup-close, .form-bg").click(function() {
     $(".pop-up").removeClass("show");
     $(".body").removeClass("fixed");
+});
+$(".testimonials__items").slick({
+    slidesToShow: 2,
+    slideToScroll: 1,
+    arrows: true,
+    dots: true,
+    infinite: true,
+    responsive: [{
+        breakpoint: 770,
+        settings: {
+            slidesToShow: 1
+        }
+    }]
 });

cdca4b7604ec86d2a6435199f1018c5d830bbed2
Pavel-Pro <38453074+Pavel-Pro@users.noreply.github.com>
2023-05-10 19:02:43 +0300

    catalog
    add button in catalog and form

--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@ $(".burger").click(function() {
     $(".menu").toggleClass("show-menu");
     $(".burger__item").toggleClass("open");
 });
+
 $(".gallery__item").click(function() {
     $(this).prependTo(".gelarry");
 });
 
-$(".button__item:last-child").click(function() {
-    $(".pop-up").addClass("show");
-    $(".body").addClass("fixed");
+$(".button__item:last-child, .catalog-card__btn").click(function() {
+    $(".pop-up, .pop-up__cataloge").addClass("show");
+    $(".body, .catalog-page").addClass("fixed");
+    console.log(2);
 });
+
 $(".popup-close, .form-bg").click(function() {
     $(".pop-up").removeClass("show");
     $(".body").removeClass("fixed");
 });
+
 $(".testimonials__items").slick({
     slidesToShow: 2,
     slideToScroll: 1,
